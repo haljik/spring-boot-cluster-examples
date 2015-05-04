8abf67d6e40eaf69af6bb24a0787dd4ff85ccf8a
haljik <se.k.hal7700@gmail.com>
2015-05-04 14:47:09 +0900

    add build system and Spring Boot Application

---
+++ src/main/java/example/Application.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by haljik on 2015/5/4.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
